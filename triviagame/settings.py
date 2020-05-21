44ce00cda2c9a8a81189a3c4653d1aafbb3e1424
kyledkearney <kyledkearney@gmail.com>
2020-05-21 14:26:27 -0600

    added rest framework info to settings.py

--- triviagame/settings.py
+++ triviagame/settings.py
@@ -103,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
